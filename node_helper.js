71ea1c17e64f91f34843fb9c151fcd86754b3d5f
sam detweiler <sdetweil@gmail.com>
2025-02-27 10:47:39 -0700

    add error handling support , due to windows message formating

--- node_helper.js
+++ node_helper.js
@@ -4,14 +4,25 @@ const express = require('express')
 module.exports = NodeHelper.create({
     socketNotificationReceived(notification, payload) {
         if (notification === 'config') {
-            this.expressApp.use(express.json());
-            this.expressApp.post("/" + payload.name, express.json({ type: '*/*' }), (req, res) => {
+            try {            
+                this.expressApp.use((err, req, res, next) => {
+                    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+                        return res.status(400).send({ status: 400, message: 'Invalid JSON payload' });
+                    }
+                    next();
+                });
+                this.expressApp.use(express.json());
+                this.expressApp.post("/" + payload.name, express.json({ type: '*/*' }), (req, res) => {
             
-                const notificationInfo = req.body
+                    const notificationInfo = req.body
 
-                this.sendSocketNotification(notificationInfo.notification, { payload: notificationInfo.payload, id: payload.id })
-                res.end()
-            });
+                    this.sendSocketNotification(notificationInfo.notification, { payload: notificationInfo.payload, id: payload.id })
+                    res.end()
+                });
+            }
+            catch (error) {
+                console.log(this.name+" message error=json formatting error")
+            }
         }
     }
 })
