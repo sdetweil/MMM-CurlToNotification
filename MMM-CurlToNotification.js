2f778189fa2bf88656641d0bcb594bcfd11fc445
sam detweiler <sdetweil@gmail.com>
2025-05-03 18:31:48 -0500

    add debugging

--- MMM-CurlToNotification.js
+++ MMM-CurlToNotification.js
@@ -1,12 +1,18 @@
 Module.register("MMM-CurlToNotification", {
     defaults:{
         url_name:"testreq",
+        debug: false
     },
-    start: function(){
-        this.sendSocketNotification('config', { name:this.config.url_name,id:this.identifier})
+    config:null,
+    notificationReceived: function (notification){
+        if(notification === 'ALL_MODULES_STARTED')
+        this.sendSocketNotification('config', { name:this.config.url_name,id:this.identifier, debug:this.config.debug})
     },
-    socketNotificationReceived: function (notification, notification_data) {
-        if(notification_data.id === this.identifier)
+    socketNotificationReceived:function(notification, notification_data){
+        if(notification_data.id === this.identifier){
+            if(this.config.debug)
+                Log.debug("received request to send notication ",notification," with payload ", notification_data.payload)
             this.sendNotification(notification, notification_data.payload)
+        }
     }
 })
--- node_helper.js
+++ node_helper.js
@@ -2,21 +2,32 @@
 const NodeHelper = require("node_helper");
 const express = require('express')
 module.exports = NodeHelper.create({
+    config:null,
     socketNotificationReceived(notification, payload) {
         if (notification === 'config') {
             try {            
+                this.config=payload
+                if(payload.debug === true)
+                    console.log(this.name+ "received request to setup")
                 this.expressApp.use((err, req, res, next) => {
                     if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+                        if(payload.debug === true){
+                            console.log(this.name+ " received request to post notification JSON data syntax error")
+                        }
                         return res.status(400).send({ status: 400, message: 'Invalid JSON payload' });
                     }
                     next();
                 });
                 this.expressApp.use(express.json());
+                if(payload.debug === true)
+                    console.log(this.name+ "setup for express route="+payload.name)
                 this.expressApp.post("/" + payload.name, express.json({ type: '*/*' }), (req, res) => {
             
                     const notificationInfo = req.body
-
-                    this.sendSocketNotification(notificationInfo.notification, { payload: notificationInfo.payload, id: payload.id })
+                    if(this.config.debug === true){
+                        console.log(this.name+ "received request to post notification ",notificationInfo , " with id="+this.config.id)
+                    }
+                    this.sendSocketNotification(notificationInfo.notification, { payload: notificationInfo.payload, id:this.config.id })
                     res.end()
                 });
             }
